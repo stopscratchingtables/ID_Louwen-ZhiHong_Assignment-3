26ff1f1e40664e5ee3c49753ba82389116ea0b4c
stopscratchingtables <hugovonlouwen@gmail.com>
2021-02-08 22:09:45 +0800

    fix

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ function createAvatar() {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        console.log("Status" + data.status + "\nAvatar SRC" + data.avatarSrc + "\nThumbnail SRC" + data.thumbnailSrc);
-    
+        console.log("Status" + data.status + "\nAvatar SRC" + data.avatarSrc + "\nThumbnail SRC" + data.thumbnailSrc + "\n" + data.doppelme_key);
+        localStorage.setItem("AvatarKey", data.doppelme_key);
+
         var imageurl = data.thumbnailSrc;
         var img = document.createElement('img');
         img.src = imageurl
         document.body.appendChild(img);
+
+        var editSkinButton = ` <button onclick="EditAvatarSkin()">Edit Skin</button> `;
+        var editAssetButton = ` <button onclick="EditAvatarAsset()">Edit Asset</button> `;
+        var delButton = ` <button onclick="DeleteAvatar()">Delete Avatar</button> `;
+        $("#avatarPic").html(img);
+        $("#avatarPic").html(editSkinButton);
+        $("#avatarPic").html(editAssetButton);
+        $("#avatarPic").html(delButton);
 
     })
     .catch(err => {
@@ -26,5 +35,63 @@ function createAvatar() {
 
 }
 
+function DeleteAvatar() {
+    var avatarKey = localStorage.getItem("AvatarKey");
+
+    fetch("https://doppelme-avatars.p.rapidapi.com/avatar/" + avatarKey, {
+	"method": "DELETE",
+	"headers": {
+		"x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
+		"x-rapidapi-host": "doppelme-avatars.p.rapidapi.com"
+	}
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+}
+
+function EditAvatarSkin()
+{
+    var avatarKey = localStorage.getItem("AvatarKey");
+
+    fetch("https://doppelme-avatars.p.rapidapi.com/avatar/" + avatarKey + "/skin/E9CBB9", {
+	"method": "PUT",
+	"headers": {
+		"x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
+		"x-rapidapi-host": "doppelme-avatars.p.rapidapi.com"
+	}
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    
+}
+
+function EditAvatarAsset()
+{
+    var avatarKey = localStorage.getItem("AvatarKey");
+    
+    fetch("https://doppelme-avatars.p.rapidapi.com/avatar/" + doppleme_key + "/" + assetID, {
+	"method": "PUT",
+	"headers": {
+		"x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
+		"x-rapidapi-host": "doppelme-avatars.p.rapidapi.com"
+	}
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    
+}
 
 // Cart API
